3d542969b8c501410c1bac84e3a4dd99458d2b00
Pira <sritharan.pirathaab@gmail.com>
2018-09-17 16:12:21 +0200

    Add java structure

---
+++ src/main/java/com/firstjavaproject/firstjavaproject/FirstJavaProjectApplication.java
@@ -0,0 +1,12 @@
+package com.firstjavaproject.firstjavaproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstJavaProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstJavaProjectApplication.class, args);
+    }
+}
